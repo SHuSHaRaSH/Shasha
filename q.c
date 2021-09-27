1c4a2fda5e9c0e6f1e78670faf3390d371953e58
zebrach77 <zebrach77@gmail.com>
2021-09-27 22:37:24 +0300

    and fib2

--- q.c
+++ q.c
@@ -8,9 +8,23 @@ int fib(int x1,int x2,int i)
     return fib(x2, x1+x2,i-1);
   }
 }
+
+
+int fib2(int i)
+{
+  int a1 = 0, a2 = 1;
+  for (int j = 0; j < i; ++j)
+  {
+    int t = a2;
+    a2 += a1;
+    a1 = t;
+
+  }
+  return(a2);
+}
 int main()
 {
   int x1;
   scanf("%d",&x1);
-  printf("%d\n",fib(0,1,x1));
+  printf("%d\n",fib2(x1));
 }
